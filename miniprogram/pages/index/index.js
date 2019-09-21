76cf70617d843a9c4e9f22304c65bd126d333b7b
jc <123345353@qq.com>
2019-09-21 16:19:21 +0800

    first page finished

---
+++ miniprogram/pages/index/index.js
@@ -0,0 +1,51 @@
+//index.js
+const app = getApp()
+
+Page({
+  data: {
+    imgUrls: [
+      '/images/swiper01.jpg',
+      '/images/swiper02.jpg',
+      '/images/swiper03.jpg'
+    ],
+    indicatorDots: false,
+    autoplay: false,
+    interval: 5000,
+    duration: 1000,
+    proList:[
+      {
+        logo:'/images/pro_01.jpg',
+        title:'标题1',
+        desc:'内容1',
+      },
+      {
+        logo: '/images/pro_02.jpg',
+        title: '标题2',
+        desc: '内容2',
+      },
+      {
+        logo: '/images/pro_03.jpg',
+        title: '标题3',
+        desc: '内容3',
+      },
+    ],
+  },
+
+  toDetail:function(e) {
+    console.log(e)
+    var index = e.currentTarget.dataset.index
+    console.log("index = "+index)
+  },
+  onLoad: function() {
+    
+  },
+
+  onGetUserInfo: function(e) {
+
+  },
+
+  onGetOpenid: function() {
+    
+  },
+
+})
